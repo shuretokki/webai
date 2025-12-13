04f6e51512c18aaf2305edb5d90bcdb4a753480e
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-13 13:29:59 +0700

    added soft deletes trait

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -3,12 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Attachment extends Model
 {
+    use SoftDeletes;
+
     protected $guarded = [];
 
-    public function message()
+    public function message(): BelongsTo
     {
         return $this->belongsTo(Message::class);
     }
