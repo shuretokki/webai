9297fae790f10a715959c4e68cad0b753c3f4397
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-13 23:36:29 +0700

    added minor docs

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -10,11 +10,21 @@ class Chat extends Model
 {
     protected $fillable = ['user_id', 'title'];
 
+    /**
+     * Summary of user
+     *
+     * @return BelongsTo<User, Chat>
+     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * Summary of messages
+     *
+     * @return HasMany<Message, Chat>
+     */
     public function messages(): HasMany
     {
         return $this->hasMany(Message::class);
