a707ec7a50f0f76823929618de3332247fc6311f
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-13 23:40:37 +0700

    added temporary middleware for delete

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,18 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->name('home');
-Route::inertia('dashboard', 'Dashboard')->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', fn () => Inertia::render('Welcome', [
+    'canRegister' => Features::enabled(Features::registration()),
+]))->name('home');
+
+Route::inertia('dashboard', 'Dashboard')->name('dashboard')
+    ->middleware(['auth', 'verified']);
+
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::delete('/chat/{chat}', [ChatController::class, 'destroy'])->name('chat.destroy');
-    Route::patch('/chat/{chat}', [ChatController::class, 'update'])->name('chat.update');
+    Route::delete('/chat/{chat}', [ChatController::class, 'destroy'])->name('chat.destroy')
+        ->middleware('can:delete, chat');
+    Route::patch('/chat/{chat}', [ChatController::class, 'update'])->name('chat.update')
+        ->middleware('can:update, chat');
     Route::post('/chat/stream', [ChatController::class, 'stream'])->name('chat.stream')
         ->middleware('throttle:chat-messages');
     Route::get('/chat', [ChatController::class, 'index'])->name('chat');
