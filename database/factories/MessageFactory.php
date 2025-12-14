28dab467fbccb579b66f12cbe42929ed2c91deba
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-14 18:13:25 +0700

    create Message factory for test data gen

---
+++ database/factories/MessageFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Chat;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Message>
+ */
+class MessageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'chat_id' => Chat::factory(),
+            'role' => fake()
+                ->randomElement(['user', 'assistant']),
+            'content' => fake()
+                ->paragraph(),
+        ];
+    }
+}
