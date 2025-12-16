d93e24c073db64a8fe9ee466634eb7287a76d18b
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-16 18:49:19 +0700

    update card component

--- resources/js/components/ui/card/Card.vue
+++ resources/js/components/ui/card/Card.vue
@@ -8,15 +8,11 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div
-    data-slot="card"
-    :class="
-      cn(
-        'bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm',
-        props.class,
-      )
-    "
-  >
+  <div data-slot="card" :class="cn(
+    'bg-card text-card-foreground flex flex-col gap-6 rounded-none border py-6 shadow-sm',
+    props.class,
+  )
+    ">
     <slot />
   </div>
 </template>
