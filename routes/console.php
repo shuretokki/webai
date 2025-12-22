b6925b45a93cc7931022a5a7bd4bc2dc65a32140
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-23 05:09:30 +0700

    update console routes

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('attachments:cleanup')->daily();
+Schedule::command('users:cleanup-unverified')->daily();
