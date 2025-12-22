a5a1bee6feaf57dbeda63bca478a883b75aa7b33
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-23 05:09:02 +0700

    update user creation

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -19,21 +19,47 @@ class CreateNewUser implements CreatesNewUsers
     public function create(array $input): User
     {
         Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['nullable', 'string', 'max:255'],
             'email' => [
                 'required',
                 'string',
                 'email',
                 'max:255',
-                Rule::unique(User::class),
+                Rule::unique(User::class)->where(function ($query) {
+                    return $query->whereNotNull('email_verified_at');
+                }),
             ],
             'password' => $this->passwordRules(),
         ])->validate();
 
+        $deletedCount = User::where('email', $input['email'])
+            ->whereNull('email_verified_at')
+            ->delete();
+
+        if ($deletedCount > 0) {
+            \Log::info('Cleaned up unverified account on registration attempt', [
+                'email' => $input['email'],
+                'deleted_count' => $deletedCount,
+            ]);
+        }
+
+        $name = $input['name'] ?? $this->generateNameFromEmail($input['email']);
+
         return User::create([
-            'name' => $input['name'],
+            'name' => $name,
             'email' => $input['email'],
             'password' => $input['password'],
         ]);
+    }
+
+    /**
+     * Generate a user-friendly name from email address.
+     */
+    protected function generateNameFromEmail(string $email): string
+    {
+        $username = explode('@', $email)[0];
+        $name = str_replace(['.', '_', '-'], ' ', $username);
+
+        return ucwords($name);
     }
 }
