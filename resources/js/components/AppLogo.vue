3391f01de4279b57acef7c5b5f0a28f4ccb91e39
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-23 05:09:13 +0700

    update app logo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -3,16 +3,9 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
 </script>
 
 <template>
-    <div class="flex items-center gap-2">
-        <div
-            class="flex aspect-square size-8 items-center justify-center rounded-none text-sidebar-primary-foreground"
-        >
-            <AppLogoIcon class="size-6 fill-current text-primary" />
-        </div>
-        <div class="grid flex-1 text-left text-sm">
-            <span class="mb-0.5 truncate leading-tight font-bold font-philosopher text-xl tracking-tight bg-gradient-to-r from-primary to-primary/70 bg-clip-text text-transparent"
-                >Ecnelis</span
-            >
-        </div>
-    </div>
+    <Link href="/explore" class="flex items-center group">
+    <img src="/assets/LOGO.svg" alt="Ecnelis" class="h-6 w-auto md:hidden group-hover:opacity-80 transition-opacity" />
+    <img src="/assets/LOGOTYPE.svg" alt="Ecnelis"
+        class="hidden md:block h-6 w-auto group-hover:opacity-80 transition-opacity" />
+    </Link>
 </template>
