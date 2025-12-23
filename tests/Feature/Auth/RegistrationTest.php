2473687b1493085e11cd52976e2337fd00f75ad4
Tri R. Utomo <tri.r.utomo@gmail.com>
2025-12-23 23:39:03 +0700

    update registration tests

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -14,6 +14,6 @@ test('new users can register', function () {
         'password_confirmation' => 'password',
     ]);
 
-    $this->assertAuthenticated();
-    $response->assertRedirect('/chat');
+    $this->assertGuest();
+    $response->assertRedirect(route('verification.notice'));
 });
